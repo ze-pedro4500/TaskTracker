1aabd4508086a8c264a862b12b9bea9d132ce170
ze-pedro4500 <64307625+ze-pedro4500@users.noreply.github.com>
2022-02-23 15:36:13 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Todo from './components/Todo';
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo />
+      <Todo />
+      <Todo />
+      <Todo />
+    </div>
+    
+  );
+}
+
+export default App;
